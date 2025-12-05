ee134e21375a07c021d892b78b9777c00f7fdbc7
Suryash Chakravarty <suryash.c@gmail.com>
2025-12-05 15:32:36 -0800

    edited scripts/02validation_splitting.py

--- scripts/01data_import.py
+++ scripts/01data_import.py
@@ -1,23 +1,3 @@
-# import pandas as pd
-# from ucimlrepo import fetch_ucirepo
-# import os
-# import click
-
-# # fetch dataset 
-# iris = fetch_ucirepo(id=53) 
-
-# iris = iris.data.original
-
-# directory = "data"
-
-# def create_dir_if_not_exists(directory):
-#         os.makedirs(directory, exist_ok=True)
-
-# create_dir_if_not_exists(directory)
-
-# iris.to_csv('data/iris.csv',index=False)
-
-
 import click
 import os
 import zipfile
