2ba73f6c405cf449dbc4224aafcb43b232db0e4c
Suryash Chakravarty <suryash.c@gmail.com>
2025-12-05 15:35:14 -0800

    final edits to 02validation_splitting.py

--- scripts/02validation_splitting.py
+++ scripts/02validation_splitting.py
@@ -1,24 +1,3 @@
-# import pandas as pd
-# import pointblank as pb
-
-
-# iris = pd.read_csv("data/iris.csv")
-# validation = (
-#     pb.Validate(iris)
-#     .col_vals_between(columns="petal width", left=0, right=5)
-#     .col_vals_between(columns="petal length", left=1, right=8)
-#     .col_vals_between(columns="sepal width", left=2, right=5)
-#     .col_vals_between(columns="sepal length", left=4, right=8)
-#     .col_exists(columns=["sepal length", "sepal width","petal length","petal width","class"])
-#     .interrogate()
-# )
-# validation
-# iris_validated = validation.get_sundered_data(type="pass")
-# iris_validated.to_csv("data/iris_validated.csv",index=False)
-
-
-
-
 import pandas as pd
 import numpy as np
 import pointblank as pb
